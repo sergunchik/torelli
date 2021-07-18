e9b6847580195db25ba69a441c0b18c610257eae
Sergey Galkin <Sergey.Galkin@phystech.edu>
2021-07-18 11:31:32 +0300

    master->main, test travis

--- _deploy.sh
+++ _deploy.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-echo 1
 [ -z "${GITHUB_PAT}" ] && exit 0
 echo 2
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "main" ] && exit 0
 echo 3
 
 git config --global user.email "torelli@galkin.org.ru"
 git config --global user.name "Sergey Galkin"
-
+echo 4
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
 git commit -m"Update the book" || true
 git push -q origin gh-pages
+echo 5
