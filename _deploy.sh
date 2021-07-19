0ba39d3ec9d258e7989a36f444756c826587c01a
Sergey Galkin <Sergey.Galkin@phystech.edu>
2021-07-19 12:41:15 +0300

    _deploy.sh works, so clean it up
    remove messages that I used for control
    give to tghe commiter bot distinctive nickname and email

--- _deploy.sh
+++ _deploy.sh
@@ -5,15 +5,11 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "main" ] && exit 0
 
-git config --global user.email "torelli@galkin.org.ru"
-git config --global user.name "Sergey Galkin"
+git config --global user.email "Travis.Torelli@galkin.org.ru"
+git config --global user.name "Travis Torelli"
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
-echo 4
 cp -r ../_book/* ./
-echo 5
 git add --all *
-git commit -m"Update the book" || true
-echo 6
+git commit -m "Update the article" || true
 git push -q origin gh-pages
-echo 7
